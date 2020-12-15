4515625bfa53115fb2ae86020cfb142c27ea5fea
Christian Mena <christian.91.gm@hotmail.com>
2020-12-15 11:16:14 -0500

    Initializing frontend project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubCalendarComponent } from './components/github-calendar/github-calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubCalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/github-calendar/github-calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-calendar',
+  templateUrl: './github-calendar.component.html',
+  styleUrls: ['./github-calendar.component.css']
+})
+export class GithubCalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
